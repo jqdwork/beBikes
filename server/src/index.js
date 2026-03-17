dd742dd008bb1bf095d3c9b939f63e48fd0044ed
Kiki testing <jqdwork@gmail.com>
2026-03-17 00:56:18 -0400

    move cors before routes

--- server/src/index.js
+++ server/src/index.js
@@ -9,15 +9,9 @@ import {
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 const allowedOrigins = ["https://white-water-0cb109f1e.2.azurestaticapps.net"];
 
 app.use(express.json());
-
-app.use("/api/products", productsRoutes);
-app.use("/api/customers", customersRoutes);
-app.use("/api/sales", salesRoutes);
-app.use("/api/salesPersons", salePersonsRoutes);
 
 app.use(
   cors({
@@ -34,6 +28,11 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
+
+app.use("/api/products", productsRoutes);
+app.use("/api/customers", customersRoutes);
+app.use("/api/sales", salesRoutes);
+app.use("/api/salesPersons", salePersonsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
